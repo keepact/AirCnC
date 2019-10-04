fa249ea03d244cc57927b457ff6d76f7d5d6d571
Renan Rollo <renanrollo@MacBook-Pro-de-Renan.local>
2019-10-04 18:42:37 -0300

    Real time with socketio and other features

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,36 @@
 const express = require('express');
-const mongoose = require ('mongoose')
+const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+
+const socketio = require('socket.io');
+const http = require('http');
+
 const routes = require('./routes');
 
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
+
 
 mongoose.connect('mongodb+srv://OminiStack:OminiStack@oministack-indr6.mongodb.net/semana09?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+})
+
+const connectedUsers = {};
+
+io.on('connection', socket => {
+    const { user_id } = socket.handshake.query;
+
+    connectedUsers[user_id] = socket.id;
+});
+
+app.use(( req, res, next ) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+
+    return next();
 })
 
 // GET, POST, PUT, DELETE
@@ -15,7 +39,9 @@ mongoose.connect('mongodb+srv://OminiStack:OminiStack@oministack-indr6.mongodb.n
 // res.params = Acessar route params (para edição e delete)
 // req.body = Acessar corpo da requisição (para criação e edição)
 
+app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
