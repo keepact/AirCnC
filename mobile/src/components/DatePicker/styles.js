112770d3096b65336bea19ce6e2b39ed198e9830
Renan Rollo <renanrollo@MacBook-Pro-de-Renan.local>
2019-11-23 22:37:13 -0300

    Adding Styles on Datapicker Component

--- mobile/src/components/DatePicker/styles.js
+++ mobile/src/components/DatePicker/styles.js
@@ -2,20 +2,15 @@ import styled from 'styled-components/native';
 
 import { RectButton } from 'react-native-gesture-handler';
 
-export const ButtonPicker = styled(RectButton)`
-  padding: 0 20px;
-  font-size: 20px;
-  color: #444;
-  height: 44px;
-  margin-bottom: 20px;
-  border-radius: 2px;
-`;
+export const ButtonPicker = styled(RectButton)``;
 
 export const InputText = styled.TextInput`
-  padding: 0 20px;
   font-size: 20px;
   color: #444;
+  background: #f5f3f2;
+  box-shadow: 10px 5px 5px black;
   height: 44px;
+  padding: 0 20px;
   margin-bottom: 20px;
-  border-radius: 2px;
+  border-radius: 4px;
 `;
