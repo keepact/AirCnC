48432e0a53222a4f5f2ba31e929bc6f280f3891c
Renan Rollo <renanrollo@MacBook-Pro-de-Renan.local>
2019-11-23 22:23:21 -0300

    Adding Android Datapicker Component

---
+++ mobile/src/components/DatePicker/styles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components/native';
+
+import { RectButton } from 'react-native-gesture-handler';
+
+export const ButtonPicker = styled(RectButton)`
+  padding: 0 20px;
+  font-size: 20px;
+  color: #444;
+  height: 44px;
+  margin-bottom: 20px;
+  border-radius: 2px;
+`;
+
+export const InputText = styled.TextInput`
+  padding: 0 20px;
+  font-size: 20px;
+  color: #444;
+  height: 44px;
+  margin-bottom: 20px;
+  border-radius: 2px;
+`;
